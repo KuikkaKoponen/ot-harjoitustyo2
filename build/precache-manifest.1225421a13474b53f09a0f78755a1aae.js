b496a58226c6cb909cc1d69dd767feb0b6b695f4
Kuikka <ville_holopainen@msn.com>
2020-05-05 17:38:16 +0300

    uibuild

---
+++ build/precache-manifest.1225421a13474b53f09a0f78755a1aae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab58246f6dae5db8c86f8e82a955e2ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20343bab1bb42877ad51",
+    "url": "/static/css/main.7efb19ac.chunk.css"
+  },
+  {
+    "revision": "96fa2a80d0443e24a6d9",
+    "url": "/static/js/2.eb02a478.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eb02a478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20343bab1bb42877ad51",
+    "url": "/static/js/main.24b5032f.chunk.js"
+  },
+  {
+    "revision": "b9db865b90dc1d93d813",
+    "url": "/static/js/runtime-main.0f618fec.js"
+  }
+]);
