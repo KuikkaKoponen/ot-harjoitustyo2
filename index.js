682e14d8cb3f573d441dad71993d9599b682c722
Kuikka <ville_holopainen@msn.com>
2020-05-05 14:53:09 +0300

    commit

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
-var morgan = require('morgan')
+const morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json()) 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
   
 morgan.token('content', (request, response) => {
-  if(request.method === 'POST') {
+  if(request.method === 'POST' || request.method === 'PUT') {
     return JSON.stringify(request.body)
   }
 })
@@ -60,8 +62,6 @@ app.post('/api/persons', (request, response) => {
   const body = request.body
   console.log(body)
   
-  //morgan.token('content', () => {return (JSON.stringify(body))})
-
   if (!body.name ) {
     return response.status(400).json({ 
       error: 'name missing' 
@@ -81,14 +81,34 @@ app.post('/api/persons', (request, response) => {
   }
 
   const note = {
+    id: Math.floor(Math.random() * 500),
     name: body.name,
-    number: body.number || 0 ,
-    id: Math.floor(Math.random() * 500)
+    number: body.number || 0
+    
   }
 
   notes = notes.concat(note)
 
   response.json(note)
+})
+
+app.put('/api/persons/:id', (request, response) => {
+  const body = request.body
+  const id = Number(request.params.id)
+  for (var i = 0; i < notes.length; i++) {
+    if (notes[i].id == id) {
+        notes[i].name = body.name
+        notes[i].number = body.number
+        console.log('Päivitetty')
+        return response.json(body)
+    }
+  }
+
+  return response.status(400).json({ 
+    error: 'Person not found' 
+  })
+
+
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -98,7 +118,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
